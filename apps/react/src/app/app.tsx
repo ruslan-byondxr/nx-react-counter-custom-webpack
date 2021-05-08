0fc278fbd3aa685aa46c3e19f4846897ec74dde9
Emily Xiong <xiongemi@gmail.com>
2021-05-07 23:19:42 -0400

    add counter at app level

--- apps/react/src/app/app.tsx
+++ apps/react/src/app/app.tsx
@@ -1,8 +1,13 @@
 import { Title, Counter, Container } from '@nx-react-counter-custom-webpack/ui';
+import { useState } from 'react';
 
 export function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <>
+      <p>You clicked the button {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Count!</button>
       <Title />
       <Counter />
       <Container>
